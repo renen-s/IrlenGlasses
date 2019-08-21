5d7f8a70a904f3c622883d90d13fe78bf531a953
Renen Sapir <ssj_r@hotmail.com>
2019-08-21 21:25:56 +0300

    basic camera flow from screen

--- app/src/main/java/com/example/irlenglasses/MainActivity.java
+++ app/src/main/java/com/example/irlenglasses/MainActivity.java
@@ -1,18 +1,37 @@
 package com.example.irlenglasses;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn;
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+        {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            btn = findViewById(R.id.button);
+            btn.setOnClickListener(new View.OnClickListener() {
 
-//    public void navigateToCamera(View view) {
-//
-//        setContentView(R.layout.activity_cam);
-//    }
+                public void onClick(View v) {
+                    Intent i = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+                    startActivity(i);
+                }
+            });
+        }
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
 }
